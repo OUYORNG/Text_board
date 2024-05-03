d2fba116ca09d800618d31a7f519fec0a093c9c8
OUYORNG <k.ouyorng@gmail.com>
2024-05-03 13:43:39 +0700

    Setup Vue Framework

---
+++ Text_Board_Vue/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+
+@media (min-width: 1024px) {
+
+}
+</style>
