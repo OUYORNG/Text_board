1f8c3adf8c919e71e61db05bf2c8867f5e8a8a4e
lymakara1 <ly.makara.personal369@gmail.com>
2024-05-08 22:41:47 +0700

    control dipslay speed

--- Backend/Js/Index.js
+++ Backend/Js/Index.js
@@ -1,5 +1,25 @@
-function changeScrollAmount() {
-  var scrollAmountInput = document.getElementById("scrollAmountInput").value;
-  var myMarquee = document.getElementById("myMarquee");
-  myMarquee.setAttribute("scrollamount", scrollAmountInput);
-}
+// const slider = document.querySelector("input");
+// const value = document.querySelector(".value");
+
+// value.textContent = slider.value;
+// slider.oninput = function () {
+//   value.textContent = this.value;
+//   document
+//     .getElementById("scroll-amount")
+//     .setAttribute("scrollamount", this.value);
+// };
+// function changeScrollAmount() {
+//   var scrollAmountInput = value.textContent;
+//   var myMarquee = document.getElementById("myMarquee");
+//   myMarquee.setAttribute("scrollamount", scrollAmountInput);
+// }
+
+const slider = document.querySelector("input");
+const value = document.querySelector(".value");
+const scrollAmount = document.getElementById("scroll-amount");
+
+value.textContent = slider.value;
+slider.oninput = function () {
+  value.textContent = this.value;
+  scrollAmount.setAttribute("scrollamount", this.value);
+};
