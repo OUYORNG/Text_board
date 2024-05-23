740611e857b84e0f6d3cc7eeeaa7f7debd6b0ad8
lymakara1 <ly.makara.personal369@gmail.com>
2024-05-24 00:14:36 +0700

    add logo

--- React_TB/src/Components/Display.jsx
+++ React_TB/src/Components/Display.jsx
@@ -1,32 +1,35 @@
-import { useContext, useState } from "react"
+import { useContext, useState } from "react";
 import { SharedText } from "./SharedText";
-import './css/left.css'
+import "./css/left.css";
 import MyComponent from "./MyComponent";
 
-function Display(){
-  const {text,setText} = useContext(SharedText)
-  const {Init} = useContext(SharedText)
-  const {direction} = useContext(SharedText)
-  const {direction1} = useContext(SharedText)
+function Display() {
+  const { text, setText } = useContext(SharedText);
+  const { Init } = useContext(SharedText);
+  const { direction } = useContext(SharedText);
+  const { direction1 } = useContext(SharedText);
 
-
-  function handle(){
-    const Input = document.querySelector(".Input").value
-    Input == "" ?  setText(Init) : setText(Input)
+  function handle() {
+    const Input = document.querySelector(".Input").value;
+    Input == "" ? setText(Init) : setText(Input);
   }
 
-return(
-        <div class="flex m-3 rounded-md flex-col ">
-            <div class="rounded-md p-10 bg-[#D9D9D9] relative" >
-                {/* <h1 className={`${direction === 1 ? "text Toright" : direction === 0? "text Toleft" : "text Stop"  } ${direction1 === 0 ? "text":"text"}`} >{text}</h1> */}
-                <MyComponent ></MyComponent>
-            </div>
-            <div class="input flex mt-2 rounded-md">
-                <input onChange={handle} class="Input flex-grow rounded-md p-4 bg-[#F0F0F0] text-xl" type="text" placeholder={Init}/>
-            </div>
-        </div>
-    )
+  return (
+    <div class="flex m-3 rounded-md flex-col ">
+      <div class="rounded-md p-10 bg-[#D9D9D9] relative">
+        {/* <h1 className={`${direction === 1 ? "text Toright" : direction === 0? "text Toleft" : "text Stop"  } ${direction1 === 0 ? "text":"text"}`} >{text}</h1> */}
+        <MyComponent></MyComponent>
+      </div>
+      <div class="input flex mt-2 rounded-md">
+        <input
+          onChange={handle}
+          class="Input flex-grow rounded-md p-4 bg-[#F0F0F0] text-xl"
+          type="text"
+          placeholder={Init}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Display;
-
--- React_TB/src/Components/Nav.jsx
+++ React_TB/src/Components/Nav.jsx
@@ -1,23 +1,49 @@
-export default function Nav(){
-    return(
-        <div class="m-3  rounded-md bg-[#71A8FB] " >
-        <div class="Nav flex-grow flex justify-between items-center pl-8 pr-8 pt-2 pb-2 md:pl-4 md:pr-4 ">   
-            <img class="w-[60px] h-[45px] rounded-sm" src=""/>  
-            <div class="about md:gap-8 md:flex hidden">
-                <a  href="" class="Home hover:text-white text-inherit">HOME</a>
-                <a  href="" class="about hover:text-white text-inherit">ABOUT US</a>
-                <a  href="" class="contact hover:text-white text-inherit">CONTACTS</a>
-            </div>
-            <div class="responsive md:hidden flex gap-3">
-                <button class="btn btn-primary full md:hidden flex justify-center items-center"><svg class="w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M8 3V5H4V9H2V3H8ZM2 21V15H4V19H8V21H2ZM22 21H16V19H20V15H22V21ZM22 9H20V5H16V3H22V9Z"></path></svg> </button>
-                <div class="aboutRes md:hidden ">
-                    <button class="relative btn btn-light menuRes flex justify-center items-center"><svg class="w-5 text-" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path></svg></button>
-                </div>
-            </div>
-            <div class="md:block hidden">
-                <button class="full ">Full Screen</button>
-            </div>
+import logo from "../images/gic-logo.png";
+
+export default function Nav() {
+  return (
+    <div class="m-3  rounded-md bg-[#71A8FB] ">
+      <div class="Nav flex-grow flex justify-between items-center pl-8 pr-8 pt-2 pb-2 md:pl-4 md:pr-4 ">
+        <img class="w-[60px] h-[45px] rounded-sm" src={logo} alt="gic logo" />
+        <div class="about md:gap-8 md:flex hidden">
+          <a href="" class="Home hover:text-white text-inherit">
+            HOME
+          </a>
+          <a href="" class="about hover:text-white text-inherit">
+            ABOUT US
+          </a>
+          <a href="" class="contact hover:text-white text-inherit">
+            CONTACTS
+          </a>
         </div>
+        <div class="responsive md:hidden flex gap-3">
+          <button class="btn btn-primary full md:hidden flex justify-center items-center">
+            <svg
+              class="w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M8 3V5H4V9H2V3H8ZM2 21V15H4V19H8V21H2ZM22 21H16V19H20V15H22V21ZM22 9H20V5H16V3H22V9Z"></path>
+            </svg>{" "}
+          </button>
+          <div class="aboutRes md:hidden ">
+            <button class="relative btn btn-light menuRes flex justify-center items-center">
+              <svg
+                class="w-5 text-"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div class="md:block hidden">
+          <button class="full ">Full Screen</button>
+        </div>
+      </div>
     </div>
-    )
+  );
 }
